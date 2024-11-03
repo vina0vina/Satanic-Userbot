349a8888bce68163a1fa5996ca877b8a26be9f53
vina0vina <vinacantik238t@gmail.com>
2024-11-03 17:20:06 +0700

    Update config.py

--- PyroUbot/config.py
+++ PyroUbot/config.py
@@ -4,14 +4,14 @@ from dotenv import load_dotenv
 
 load_dotenv(".env")
 
-API_ID: int = os.getenv('API_ID', 26666537)
-API_HASH: str = os.getenv('API_HASH', "6c2f0f671fbbfbfff8bca871a69c5692")
+API_ID: int = os.getenv('API_ID', 22353526)
+API_HASH: str = os.getenv('API_HASH', "ca3bc21bc1f8970fc12ad76790ff8e0e")
 
-BOT_TOKEN = os.getenv("7648455672:AAEJrICgoCT9TxbpfJGxLEZsXLrC0FgGrQU")
+BOT_TOKEN = os.getenv("7331678485:AAGvdOPCUwxP3y-5-feW-BlRyT7juo-a-6E")
 
-OWNER_ID = int(os.getenv("7239918330"))
+OWNER_ID = int(os.getenv("6960845020"))
 
-LOGS_MAKER_UBOT = int(os.getenv("-1002191896661"))
+LOGS_MAKER_UBOT = int(os.getenv("-4590999823"))
 
 MAX_BOT = int(os.getenv("9999"))
 
